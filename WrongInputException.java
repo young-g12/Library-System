f4b9a11668e3bdead222cdd6a99e96d60b1b03da
Gil <gmiranda540@yahoo.com>
2025-12-13 10:43:45 -0500

    Custom Exception class for incorrect input

---
+++ WrongInputException.java
@@ -0,0 +1,7 @@
+public class WrongInputException extends Exception {
+
+    public WrongInputException() {
+        super();
+        System.out.println("Entered the wrong type of information.");
+    }
+}
