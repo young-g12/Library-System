5f58b8a51099a16ade6d8783491f4ed1295f3525
Gil <gmiranda540@yahoo.com>
2025-12-25 10:31:38 -0500

    Needs work
    q
    q

--- Patron.java
+++ Patron.java
@@ -13,40 +13,35 @@ public class Patron {
 
     public String getName() {
         return name;
-    }
+    } 
 
 
     public void limitsBooks() {
-        if (books.size() == 3) {
-            System.out.println("Sorry, you can no longer checkout a book");
-            System.out.println("You may return one or more in order to continue checkout");
+        int j = 0;
+        while (books.size() < 3) {
             
-	    System.out.println("Enter book you'd like to check out: ");
-        String title = scanner.nextLine();
+            System.out.println("Enter book you'd like to check out: ");
+            String title = scanner.nextLine();
 
-        boolean found = false;
-
-        int count = 3;
+            boolean found = false;
         
-
             for (int i = 0; i < books.size(); i++) {
             Book b = books.get(i);
-            if (b.getTitle().equalsIgnoreCase(title)) {
-                books.remove(i);
-                System.out.println(title + " has been checked out.");
-                found = true;
-                break;
+                if (b.getTitle().equalsIgnoreCase(title)) {
+                    books.remove(i);
+                    System.out.println(title + " has been checked out.");
+                    found = true;
+                    j--;
+                    break;
+                }
+            } 
+
+            if (!found) {
+                System.out.println("Book is not available.");
             }
-            count--;
-            System.out.println("Cannot check out any more books");
-          System.out.println(count);
-          } //end of for loop
-
-          if (!found) {
-            System.out.println("Book is not available.");
-          }
-
-        }
+        } 
+                System.out.println("Sorry, you can no longer checkout a book");
+                System.out.println("You may return one or more in order to continue checkout");
+        
     }
-
 }
