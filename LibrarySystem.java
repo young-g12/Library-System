51b1423585cb9e7e6f77a11b7936e9ea817d4e63
Gil <gmiranda540@yahoo.com>
2026-01-04 10:48:34 -0500

    proper error handling/custom exception created; implemented end program method; switched to switch statements rather than if/else for conditionals

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,7 +1,13 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class LibrarySystem {
+    /**
+     * @param args
+     * @throws IOException
+     * @throws WrongInputException
+     */
     public static void main(String[] args) throws IOException, WrongInputException {
         Book book = new Book();
         Patron p  = new Patron();
@@ -19,28 +25,56 @@ public class LibrarySystem {
             System.out.println("5. Display books in system");
             System.out.println();
             System.out.print("Enter choice: ");
-            int choice = scanner.nextInt();
+            int choice;
+            
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                continue;
+            }
+
+             if (choice < 0 || choice > 5) {
+                throw new WrongInputException();
+            }
 
             //ends program
-            if (choice ==0) {
-                System.out.println("Exiting Library System");
-                break;
-            }
+            // if (choice ==0) {
+            //     System.out.println("Exiting Library System");
+            //     break;
+            // }
 
-            if (choice == 1) {
-                book.findBook();
-            } else if (choice == 2) {
-                book.addBook();
-            } else if (choice == 3) {
-                book.checkOutBook();
-                // p.limitsBooks();
-            } else if (choice == 4) {
-                book.returnBook();
-            } else if(choice == 5) {
-                book.displayAllBooks();
-            } else {
-                System.out.println("Invalid option, please try again");
+            // if (choice == 1) {
+            //     book.findBook();
+            // } else if (choice == 2) {
+            //     book.addBook();
+            // } else if (choice == 3) {
+            //     book.checkOutBook();
+            //     // p.limitsBooks();
+            // } else if (choice == 4) {
+            //     book.returnBook();
+            // } else if(choice == 5) {
+            //     book.displayAllBooks();
+            // } else {
+            //     System.out.println("Invalid option, please try again");
+            // }
+
+            switch (choice) {
+                case 0 -> endProgram();
+                case 1 -> book.findBook();
+                case 2 -> book.addBook();
+                case 3 -> book.checkOutBook();
+                case 4 -> book.returnBook();
+                case 5 -> book.displayAllBooks();
+                default -> System.out.println("Invalid iption, please try again");
             }
+        
         }
+    }
+
+    public static void endProgram() {
+            System.out.println("Exiting Library System...");
+            System.exit(0);
     }
 }
