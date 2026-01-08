758ea7a31d33e35af7f4cc03ab60cece28ba2455
Gil <gmiranda540@yahoo.com>
2026-01-08 09:16:03 -0500

    user can switch between patron and admin portals; created login for admin

--- LibraryAdmin.java
+++ LibraryAdmin.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LibraryAdmin {
@@ -11,7 +12,7 @@ public class LibraryAdmin {
 
     }
 
-    public void adminLogin() throws IOException {
+    public void adminLogin() throws IOException, WrongInputException {
         while (passwordAttempts <= 3) {
                 System.out.println("Enter password: ");
                 password = scanner.nextInt();
@@ -26,13 +27,15 @@ public class LibraryAdmin {
         endProgram();
     }
 
-    public void displayAdminCommands() throws IOException {
+    public void displayAdminCommands() throws IOException, WrongInputException {
         
         while (true) {
             System.out.println("0. Exit program");
             System.out.println("1. Search Book by Title");
             System.out.println("2. Display Books in System");
             System.out.println("3. Add Books to Library");
+            System.out.println("4. Patron Portal");
+            System.out.println();
             System.out.println("Select command: ");
             int choice = scanner.nextInt();
             
@@ -41,10 +44,58 @@ public class LibraryAdmin {
                 case 1 -> book.findBook();
                 case 2 -> book.displayAllBooks();
                 case 3 -> book.addBook();
+                case 4 -> MainMenu();
                 default -> System.out.println("Invalid option, please try again");
             }
         }
     }
+
+    private Object MainMenu() throws WrongInputException, IOException {
+        Book book = new Book();
+        Patron p = new Patron();
+        LibraryAdmin admin = new LibraryAdmin();
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("---Welcome to Library System---");
+        
+        while (true) {
+
+            System.out.println("0. Exit Program");
+            System.out.println("1. Checkout Books");
+            System.out.println("2. Return book");
+            System.out.println("3. Display Books in System");
+            System.out.println("4. Create Patron Book Log");
+            System.out.println("5. Display Patrons in system");
+            System.out.println("6. Admin Portal");
+            System.out.println();
+            System.out.print("Select command: ");
+            int choice;
+            
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                continue;
+            }
+
+             if (choice < 0 || choice > 6) {
+                throw new WrongInputException();
+            }
+
+            switch (choice) {
+                case 0 -> endProgram();
+                case 1 -> book.checkOutBook();
+                case 2 -> book.returnBook();
+                case 3 -> book.displayAllBooks();
+                case 4 -> p.createPatron();
+                case 5 -> p.searchPatrons();
+                case 6 -> admin.adminLogin();
+                default -> System.out.println("Invalid option, please try again");
+            }
+        
+        }
+    }
+    
 
     public static void endProgram() {
             System.out.println("Exiting Library System...");
--- LibrarySystem.java
+++ LibrarySystem.java
@@ -19,9 +19,9 @@ public class LibrarySystem {
         System.out.println("Admin(1) or User(2)");
         int input;
         input = scanner.nextInt();
+
         if (input == 1) {
             admin.adminLogin();
-            // admin.displayAdminCommands();
         } else {
 
         }
@@ -30,15 +30,14 @@ public class LibrarySystem {
         System.out.println("---Welcome to Library System---");
         
         while (true) {
-            
-            // System.out.println("1. Search Book by Title");
-            // System.out.println("2. Add Books");
 
-            System.out.println("3. Checkout Books");
-            System.out.println("4. Return book");
-            System.out.println("5. Display Books in System");
-            System.out.println("6. Create Patron Book Log");
-            System.out.println("7. Display Patrons in system");
+            System.out.println("0. Exit Program");
+            System.out.println("1. Checkout Books");
+            System.out.println("2. Return book");
+            System.out.println("3. Display Books in System");
+            System.out.println("4. Create Patron Book Log");
+            System.out.println("5. Display Patrons in system");
+            System.out.println("6. Admin Portal");
             System.out.println();
             System.out.print("Select command: ");
             int choice;
@@ -51,24 +50,18 @@ public class LibrarySystem {
                 continue;
             }
 
-             if (choice < 0 || choice > 7) {
+             if (choice < 0 || choice > 6) {
                 throw new WrongInputException();
             }
 
             switch (choice) {
                 case 0 -> endProgram();
-                //for library staff
-                case 1 -> book.findBook();
-                case 5 -> book.displayAllBooks();
-                case 2 -> book.addBook();
-
-                // for patrons
-                case 6 -> p.createPatron();
-                case 3 -> book.checkOutBook();
-                case 4 -> book.returnBook();
-                case 7 -> p.searchPatrons();
-                
-                
+                case 1 -> book.checkOutBook();
+                case 2 -> book.returnBook();
+                case 3 -> book.displayAllBooks();
+                case 4 -> p.createPatron();
+                case 5 -> p.searchPatrons();
+                case 6 -> admin.adminLogin();
                 default -> System.out.println("Invalid option, please try again");
             }
         
@@ -79,4 +72,50 @@ public class LibrarySystem {
             System.out.println("Exiting Library System...");
             System.exit(0);
     }
+
+    // public void MainMenu() throws WrongInputException, IOException {
+    //     Book book = new Book();
+    //     Patron p = new Patron();
+    //     LibraryAdmin admin = new LibraryAdmin();
+    //     Scanner scanner = new Scanner(System.in);
+    //      System.out.println("---Welcome to Library System---");
+        
+    //     while (true) {
+
+    //         System.out.println("0. Exit Program");
+    //         System.out.println("1. Checkout Books");
+    //         System.out.println("2. Return book");
+    //         System.out.println("3. Display Books in System");
+    //         System.out.println("4. Create Patron Book Log");
+    //         System.out.println("5. Display Patrons in system");
+    //         System.out.println("6. Admin Portal");
+    //         System.out.println();
+    //         System.out.print("Select command: ");
+    //         int choice;
+            
+    //         try {
+    //             choice = scanner.nextInt();
+    //             scanner.nextLine();
+    //         } catch (InputMismatchException e) {
+    //             scanner.nextLine();
+    //             continue;
+    //         }
+
+    //          if (choice < 0 || choice > 6) {
+    //             throw new WrongInputException();
+    //         }
+
+    //         switch (choice) {
+    //             case 0 -> endProgram();
+    //             case 1 -> book.checkOutBook();
+    //             case 2 -> book.returnBook();
+    //             case 3 -> book.displayAllBooks();
+    //             case 4 -> p.createPatron();
+    //             case 5 -> p.searchPatrons();
+    //             case 6 -> admin.adminLogin();
+    //             default -> System.out.println("Invalid option, please try again");
+    //         }
+        
+    //     }
+    // }
 }
